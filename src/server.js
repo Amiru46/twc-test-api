1bc4bb20ccb92df57045d261cf2d94dbef81c8bd
Amiru46 <165811854+Amiru46@users.noreply.github.com>
2025-03-16 17:01:43 +0530

    Done
    Success! my MongoDB connection is now working.

--- src/server.js
+++ src/server.js
@@ -1,11 +1,12 @@
 const express = require("express");
+require("dotenv").config();
 const mongoose = require("mongoose");
-const authRoutes = require("./routes/authRoutes"); // Correct path
-const contactRoutes = require("./routes/contactRoutes"); // Correct path
-const connectDB = require("./config/db"); // Correct path
+const authRoutes = require("./routes/authRoutes");
+const contactRoutes = require("./routes/contactRoutes"); 
+const connectDB = require("./config/db");
 const cors = require("cors");
 
-
+console.log("MONGO_URI:", process.env.MONGO_URI);
 const app = express();
 
 
@@ -23,6 +24,6 @@ app.use((err, req, res, next) => {
     res.status(500).send('Something broke!');
 });
 
-app.listen(5000, () => {
+app.listen(5001, () => {
   console.log("Server running on port 5000");
 });
